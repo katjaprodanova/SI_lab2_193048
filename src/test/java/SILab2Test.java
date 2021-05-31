d17bcf86426ecd4729f26a9b520b612a2ea84287
unknown <katja.prodanova.001@gmail.com>
2021-05-31 23:14:00 +0200

    initial commit

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+     private final SILab2 function = new SILab2();
+  public static List<Time> createList(int a,int b,int c){
+      List<Time> timeList = new ArrayList<>(new Time(a,b,c));
+      return timeList;
+  }
+    @Test
+    void function() {
+    }
+    @Test
+    void EveryBranch(){
+
+
+
+    }
+    @Test
+    void MultipleCondition(){
+
+    }
+}
